abe8a973fc75590900c6f3ef3ebaa91307205247
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-11-12 00:22:58 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "web-server-for-app01" {
   provisioner "local-exec" {
     command = "echo ansible_host=${aws_instance.web-server-for-app01.private_ip} ansible_user=ubuntu  ansible_connection=ssh  ansible_become=yes >> hosts"
   }
-  }
+  
 }
 
